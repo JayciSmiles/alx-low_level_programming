378aea8ebc705fe40fb59ee85a1b31c7a19ea046
Chisom Eze <ezejessica147@gmail.com>
2023-02-18 20:56:05 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: Alaways  0
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (c = 'a'; c <= 'h'; c++)
+		putchar (c);
+
+	putchar('\n');
+
+	return (0);
+}
