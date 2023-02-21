ef6c5a15aebd22b68fb1ac8ad35e25e7906c0b49
Chisom Eze <ezejessica147@gmail.com>
2023-02-20 19:08:24 -0800

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,6 @@ int main(void)
 	printf("size of a long int; %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int; %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float; %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
