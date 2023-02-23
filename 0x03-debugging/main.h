869280c1d0f34ca97c28559cd92da09b8d6ea08f
Chisom Eze <ezejessica147@gmail.com>
2023-02-23 10:20:33 -0800

    largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 
-void postitive_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif /* MAIN_H */
+#endif
