c38e524e62966f6012e51f1ac72a341101255300
Chisom Eze <ezejessica147@gmail.com>
2023-02-23 12:26:34 -0800

    0 main c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
+*
 * Return: 0
 */
 int main(void)
@@ -8,6 +9,7 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
